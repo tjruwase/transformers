0026e9853f41a14e6c7c32374199c99f671df39f
Cheng Li <pistasable@gmail.com>
2023-06-23 00:01:01 -0700

    add bloom

--- src/transformers/models/opt/modeling_opt.py
+++ src/transformers/models/opt/modeling_opt.py
@@ -658,7 +658,6 @@ class OPTDecoder(OPTPreTrainedModel):
                 Whether or not to return a [`~utils.ModelOutput`] instead of a plain tuple.
         """
         is_decoding_stage = past_key_values is not None
-
         output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
         output_hidden_states = (
             output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
